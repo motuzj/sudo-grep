621a6aa6af0dcf40400d549198ea5641888c7246
Juraj Motuz <motuzjuraj@gmail.com>
2023-05-18 12:00:09 +0200

    getline()

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void) {
-	char c;
-	while ((c = getchar()) != EOF) {
-		printf("%c", c);
+	char *str = NULL;
+	size_t n = 0;
+	while (getline(&str, &n, stdin) != -1) {
+		printf("%s", str);
 	}
+	free(str);
 	return 0;
 }
