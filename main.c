37b8a04591253e111634e2eeb3f656b3adfd604f
Juraj Motuz <motuzjuraj@gmail.com>
2023-05-18 13:35:11 +0200

    error handling

--- main.c
+++ main.c
@@ -1,12 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
 int main(void) {
-	char *str = NULL;
-	size_t n = 0;
-	while (getline(&str, &n, stdin) != -1) {
-		printf("%s", str);
-	}
-	free(str);
-	return 0;
+    char *str = NULL;
+    size_t n = 0;
+    errno = 0;
+
+    while (getline(&str, &n, stdin) != -1) {
+        printf("%s", str);
+    }
+    if (errno != 0) {
+        printf("%s\n", strerror(errno));
+        free(str);
+        return 1;
+    }
+
+    free(str);
+    return 0;
 }
