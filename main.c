f9d2c8bad03b68a7dc72b74a522475449388c9fd
Juraj Motuz <motuzjuraj@gmail.com>
2023-05-19 19:09:53 +0200

    simpler code

--- main.c
+++ main.c
@@ -5,36 +5,31 @@
 #include <errno.h>
 
 // prints only lines that contain "word"
-int print_line_with_word(char *word, char *line) {
-    char *p_word = line; // storing pointer to "word"
-    int printed = 0;
-
-    while ((p_word = strstr(p_word, word)) != NULL) {
-        if (!isatty(STDOUT_FILENO) || !strcmp(word, "")) { // if stdout is terminal or "word" is empty
-            printf("%s", line);
-            return 0;
+int print_line_with_word(char *word, const char *line, int is_tty) {
+    int found_cnt = 0;
+    const char *search_start = line; // storing pointer to "word"
+    while (search_start && *search_start) {
+        const char *found_word = strstr(search_start, word);
+        if (found_word) {
+            found_cnt++;
+            for (; search_start < found_word; search_start++) {
+                printf("%c", *search_start);
+            }
+            if (is_tty) {
+                printf("\033[32m"); // set green color
+            }
+            printf("%s", word);
+            if (is_tty) {
+                printf("\033[0m"); // reset color
+            }
+            search_start += strlen(word);
+        } else {
+            break;
         }
-        int position = p_word - line;
-
-        // print characters before "word"
-        for (int i = printed; i < position; i++) {
-            printf("%c", line[i]);
-        }
-
-        printf("\033[32m"); // set green color
-        for (int i = position; i < position + strlen(word); i++) {
-            printf("%c", line[i]); // print word
-        }
-        printf("\033[0m"); // reset color
-
-        printed = position + strlen(word);
-        p_word += strlen(word);
     }
-    if (printed) {
+    if (found_cnt > 0) {
         // print remaining characters
-        for (int i = printed; i < strlen(line); i++) {
-            printf("%c", line[i]);
-        }
+        printf("%s", search_start);
     }
     return 0;
 }
@@ -49,6 +44,7 @@ int main(int argc, char *argv[]) {
 
     char *str = NULL;
     size_t n = 0;
+    int is_tty = isatty(STDOUT_FILENO);
     errno = 0;
 
     while (getline(&str, &n, stdin) != -1) {
@@ -56,8 +52,7 @@ int main(int argc, char *argv[]) {
             fprintf(stderr, "Error: %s\n", strerror(errno));
             break;
         }
-        print_line_with_word(word, str);
-        errno = 0; // clear ENOTTY error caused by isatty()
+        print_line_with_word(word, str, is_tty);
     }
 
     free(str);
