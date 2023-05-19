c963e2affa76dc970d5e958b2e93d580f384f2e2
Juraj Motuz <motuzjuraj@gmail.com>
2023-05-19 13:24:47 +0200

    green highlighting for matches

--- main.c
+++ main.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <errno.h>
 
-// prints only lines that contains "word"
+// prints only lines that contain "word"
 int print_line_with_word(char *word, char *line) {
-    if (strstr(line, word) != NULL) {
-        printf("%s", line);
+    char *p_word; // storing pointer to "word"
+
+    if ((p_word = strstr(line, word)) != NULL) {
+        if (isatty(STDOUT_FILENO)) { // if stdout is terminal
+            int position = p_word - line;
+
+            for (int i = 0; i < strlen(line); i++) {
+                if (i >= position && i <= position + strlen(word) - 1) {
+                    printf("\033[32m%c\033[0m", line[i]);
+                } else {
+                    printf("%c", line[i]);
+                }
+            }
+        } else {
+            printf("%s", line);
+        }
     }
     return 0;
 }
@@ -24,14 +39,14 @@ int main(int argc, char *argv[]) {
     errno = 0;
 
     while (getline(&str, &n, stdin) != -1) {
+        if (errno != 0) {
+            fprintf(stderr, "Error: %s\n", strerror(errno));
+            break;
+        }
         print_line_with_word(word, str);
+        errno = 0; // clear ENOTTY error caused by isatty()
     }
 
     free(str);
-
-    if (errno != 0) {
-        fprintf(stderr, "Error: %s\n", strerror(errno));
-        return 1;
-    }
     return 0;
 }
